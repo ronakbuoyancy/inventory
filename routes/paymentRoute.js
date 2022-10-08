53a58147ff29334f1c7f3a0d1209bfcff8ee6b8b
Ronak Patel <ronak.buoyancy@gmail.com>
2022-10-08 16:26:52 +0530

    payment api change

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const {createPayment, getPaymentsByCustomer, updatePayment, deletePayment, getPaymentDetails} = require("../controllers/PaymentController");
+const {createPayment, getPaymentsByCustomer, updatePayment, deletePayment, getPaymentDetails} = require("../controllers/paymentController");
 
 const router = express.Router();
 
